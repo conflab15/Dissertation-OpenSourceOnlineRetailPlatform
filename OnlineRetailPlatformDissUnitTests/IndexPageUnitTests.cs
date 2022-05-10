fd073a7a035b02364bc4cb2e8adfbcf86d112de9
Connor Clarke <connorclarke1511@hotmail.com>
2022-05-10 22:08:59 +0100

    Implementation of Unit Testing using bUnit and xUnit

---
+++ OnlineRetailPlatformDissUnitTests/IndexPageUnitTests.cs
@@ -0,0 +1,108 @@
+﻿using Xunit;
+using Bunit;
+using OnlineRetailPlatformDiss.Pages.Business;
+using OnlineRetailPlatformDiss.Pages.Customer;
+using OnlineRetailPlatformDiss.Data;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineRetailPlatformDiss.Services;
+
+namespace OnlineRetailPlatformDissUnitTests
+{
+    public class IndexPageUnitTests
+    {
+        [Fact]
+        public void IndexPageBusinessOptionsRendersWithUsername()
+        {
+            //Test - BusinessManagerOptions renders correctly when a username is passed via the parameters
+            //Arrange
+            using var ctx = new TestContext();
+
+            //Act
+            var cut = ctx.RenderComponent<BusinessManagerOptions>(parameters => parameters
+            .Add(p => p.UserForename, "testuser@test.com"));
+
+            //Assert
+            cut.Markup.Contains("<h1 pb-2>Welcome back, testuser@test.com, Business Management Options are shown below!</h1>");
+        }
+
+        [Fact]
+        public void IndexPageSearchProductsRendersCorrectly()
+        {
+            //Test - Checks if the SearchProducts component loads correctly...
+            //Arrange
+            using var ctx = new TestContext();
+
+            //Registering Services.
+            ctx.Services.AddSingleton<ApplicationDbContext>(new ApplicationDbContext());
+            ctx.Services.AddScoped<AppState>();
+            ctx.Services.AddScoped<ShoppingBasketService>();
+
+            //Act
+            var cut = ctx.RenderComponent<SearchProducts>();
+
+            //Assert
+            cut.Markup.Contains("<h2>Sort by:</h2>");
+        }
+
+        [Fact]
+        public void IndexPageSearchProductsRendersWithSearchCriteria()
+        {
+            //Test - Checks if the Search Criteria is applied when the button is clicked...
+            //Arrange
+            using var ctx = new TestContext();
+
+            //Registering Services.
+            ctx.Services.AddSingleton<ApplicationDbContext>(new ApplicationDbContext());
+            ctx.Services.AddScoped<AppState>();
+            ctx.Services.AddScoped<ShoppingBasketService>();
+
+            //Act
+            var cut = ctx.RenderComponent<SearchProducts>();
+            cut.Find("input[name=SearchString]").Change("Knitted");
+            cut.Find("input[value=Search]").Click();
+
+            //Assert
+            cut.Markup.Contains("<h2>Search Criteria: 'Knitted' </h2>");
+        }
+
+        [Fact]
+        public void IndexPageSearchProductsFiltersByPrice()
+        {
+            //Test - Checks if the Filter by price is applied when the button is clicked 
+            //Arrange
+            using var ctx = new TestContext();
+
+            //Registering Services.
+            ctx.Services.AddSingleton<ApplicationDbContext>(new ApplicationDbContext());
+            ctx.Services.AddScoped<AppState>();
+            ctx.Services.AddScoped<ShoppingBasketService>();
+
+            //Act
+            var cut = ctx.RenderComponent<SearchProducts>();
+            cut.Find("a:contains('Price (Ascending)')").Click();
+
+            //Assert
+            cut.Markup.Contains("<h2>Sorted by Price (Ascending)</h2>");
+        }
+
+        [Fact]
+        public void IndexPageSearchProductsFiltersByAZ()
+        {
+            //Test - Checks if the Filter by A-Z is applied when the button is clicked 
+            //Arrange
+            using var ctx = new TestContext();
+
+            //Registering Services.
+            ctx.Services.AddSingleton<ApplicationDbContext>(new ApplicationDbContext());
+            ctx.Services.AddScoped<AppState>();
+            ctx.Services.AddScoped<ShoppingBasketService>();
+
+            //Act
+            var cut = ctx.RenderComponent<SearchProducts>();
+            cut.Find("a:contains('Title (A-Z)')").Click();
+
+            //Assert
+            cut.Markup.Contains("<h2>Sorted by Title (A-Z)</h2>");
+        }
+    }
+}
---
+++ OnlineRetailPlatformDissUnitTests/ShoppingBasketPageUnitTests.cs
@@ -0,0 +1,122 @@
+﻿using Xunit;
+using Bunit;
+using OnlineRetailPlatformDiss.Pages.Business;
+using OnlineRetailPlatformDiss.Pages.Customer;
+using OnlineRetailPlatformDiss.Data;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineRetailPlatformDiss.Services;
+using Microsoft.AspNetCore.Components;
+using System.Linq;
+
+namespace OnlineRetailPlatformDissUnitTests
+{
+    public class ShoppingBasketPageUnitTests
+    {
+        [Fact]
+        public void ShoppingBasketPageRendersWhenEmpty()
+        {
+            //Test - Checks that the empty basket message loads when the basket is empty
+            //Arrange
+            using var ctx = new TestContext();
+
+            //Registering Services.
+            ctx.Services.AddSingleton<ApplicationDbContext>(new ApplicationDbContext());
+            ctx.Services.AddScoped<AppState>();
+            ctx.Services.AddScoped<ShoppingBasketService>();
+            ctx.Services.GetRequiredService<NavigationManager>();
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+
+            //Act
+            var cut = ctx.RenderComponent<ShoppingBasket>();
+
+            //Assert
+            cut.Markup.Contains("It looks like you haven't added any products to your basket! Please add something!");
+        }
+
+        [Fact]
+        public void ShoppingBasketRendersWithItemPresent()
+        {
+            //Test - Checks if the basket displays the BasketProductCard when an item is added...
+            //Arrange
+            using var ctx = new TestContext();
+
+            //Registering Services
+            ctx.Services.AddSingleton<ApplicationDbContext>(new ApplicationDbContext());
+            ctx.Services.AddScoped<AppState>();
+            ctx.Services.AddScoped<ShoppingBasketService>();
+            ctx.Services.GetRequiredService<NavigationManager>();
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+
+            //Act
+            var cutProduct = ctx.RenderComponent<SearchProducts>();
+            cutProduct.Find("a:contains('Add to Basket')").Click();
+
+            var cut = ctx.RenderComponent<ShoppingBasket>();
+
+            //Assert - Markup should contain the message when a product is present in basket...
+            cut.Markup.Contains("Total Items: 1");
+        }
+
+        [Fact]
+        public void ShoppingBasketEmptiesBasketWhenEmptyBasketIsPressed()
+        {
+            //Test - Count returns to zero when Empty Basket is pressed
+            //Arrange
+            using var ctx = new TestContext();
+
+            //Registering Services
+            ctx.Services.AddSingleton<ApplicationDbContext>(new ApplicationDbContext());
+            ctx.Services.AddScoped<AppState>();
+            ctx.Services.AddScoped<ShoppingBasketService>();
+            ctx.Services.GetRequiredService<NavigationManager>();
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+
+            //Act
+            var cutProduct = ctx.RenderComponent<SearchProducts>();
+            cutProduct.Find("a:contains('Add to Basket')").Click();
+
+            var cut = ctx.RenderComponent<ShoppingBasket>();
+
+            var aTags = cut.FindAll("a");
+            var button = aTags.FirstOrDefault(a => a.ParentElement.TextContent.Contains("Empty Basket"));
+            button?.Click();
+
+            var divs = cut.FindAll("div");
+            var deleteBtn = divs.FirstOrDefault(a => a.ParentElement.TextContent.Contains("Yes, empty the basket!"));
+            deleteBtn?.Click();
+
+            //Assert - Markup should contain the message when basket is empty
+            cut.Markup.Contains("Total Items: 0");
+        }
+
+        [Fact]
+        public void ShoppingBasketCheckoutFailsWhenEmptyBasket()
+        {
+            //Test - A warning should appear when the checkout button is pressed with an empty basket
+            //Arrange
+            var ctx = new TestContext();
+
+            //Registering Services
+            ctx.Services.AddSingleton<ApplicationDbContext>(new ApplicationDbContext());
+            ctx.Services.AddScoped<AppState>();
+            ctx.Services.AddScoped<ShoppingBasketService>();
+            ctx.Services.GetRequiredService<NavigationManager>();
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+
+            //Act
+            var cut = ctx.RenderComponent<ShoppingBasket>();
+
+            //Find all Buttons with an a tag
+            var aTags = cut.FindAll("a");
+            var button = aTags.FirstOrDefault(a => a.ParentElement.TextContent.Contains("Checkout"));
+            button?.Click();
+
+            //Find all div elements (used for swal2 alerts)
+            var divs = cut.FindAll("div");
+            var warning = divs.FirstOrDefault(a => a.ParentElement.TextContent.Contains("You need an item in your basket to checkout!")).ToString();
+
+            //Assert
+            cut.Markup.Contains(warning);
+        }
+    }
+}
